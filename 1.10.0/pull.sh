77cd0793617cefcb28df6cfeea1ba606bea33790
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-09-21 20:39:11 -0400

    Updated script

--- 1.10.0/pull.sh
+++ 1.10.0/pull.sh
@@ -4,5 +4,5 @@ if ! [ -x "$(command -v git)" ]; then
 	module load singularity/3.5.3
 fi
 
-singularity pull https://depot.galaxyproject.org/singularity/samtools:1.2-0
-mv -v samtools:1.2-0 singularity-samtools-1.2-0.sif
+singularity pull https://depot.galaxyproject.org/singularity/samtools:1.10--h9402c20_2
+mv -v samtools:1.10--h9402c20_2 singularity-samtools-1.10.0.sif
