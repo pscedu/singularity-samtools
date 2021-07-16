c34aba31997026b91e4bf110107242be868c7813
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-16 13:09:27 -0400

    Update modulefile.lua

--- 1.10.0/modulefile.lua
+++ 1.10.0/modulefile.lua
@@ -13,23 +13,28 @@ whatis("URL: https://www.psc.edu/resources/software")
 whatis("Description: Samtools is a suite of programs for interacting with high-throughput sequencing data.")
 
 help([[
-Samtools 1.10.0
---------------
-Description
------------
 Samtools is a suite of programs for interacting with high-throughput sequencing data.
+  
 To load the module type
+  
 > module load samtools/1.10.0
+  
 To unload the module type
+  
 > module unload samtools/1.10.0
+  
 Documentation
 -------------
 http://www.htslib.org/doc/#manual-pages
+  
 For help, type
+  
 > samtools --help
+  
 Repository
 ----------
 https://github.com/samtools/samtools
+  
 Tools included in this module are
 * samtools
 ]])
